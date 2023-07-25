4ea0f25ce416903644c38d66f7c61136514d93e5
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-25 14:44:52 +0300

    Set up shipdisplay component

--- frontend/src/app/components/shipdisplay/shipdisplay.component.ts
+++ frontend/src/app/components/shipdisplay/shipdisplay.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Ships } from 'src/app/data/shipdata';
+import { ShipsService } from 'src/app/services/ships.service';
 
 @Component({
   selector: 'app-shipdisplay',
   templateUrl: './shipdisplay.component.html',
-  styleUrls: ['./shipdisplay.component.scss']
+  styleUrls: ['./shipdisplay.component.scss'],
 })
-export class ShipdisplayComponent {
+export class ShipdisplayComponent implements OnInit {
+  ships!: Ships[];
 
+  @Output() itemIsClicked = new EventEmitter<Ships>();
+
+  constructor(private shipService: ShipsService) {}
+
+  ngOnInit(): void {
+    this.ships = this.shipService.getAll();
+    this.ships = this.ships.map(function (e) {
+      e['imagePath'] = 'assets/' + e.imageFileName;
+      return e;
+    });
+    this.itemIsClicked.emit(this.ships[0]);
+  }
 }
