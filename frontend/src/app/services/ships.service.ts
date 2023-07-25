e60b28c5ea248c9d2c7d9aaee03b38ceddc37729
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-25 14:34:47 +0300

    Set up ship service

--- frontend/src/app/services/ships.service.ts
+++ frontend/src/app/services/ships.service.ts
@@ -1,9 +1,15 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Injectable } from '@angular/core';
+import { Ships } from '../data/shipdata';
+import { Ship } from '../data/shipdescription';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShipsService {
   constructor() {}
+
+  getAll(): Ships[] {
+    return Ship;
+  }
 }
