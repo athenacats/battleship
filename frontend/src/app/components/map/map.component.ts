abb149123fe2a5e5c23d3f48c63935d94c23a791
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-26 09:35:13 +0300

    Set up map grid and style

--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ships } from 'src/app/data/shipdata';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  rows = Array.from({ length: 10 }, (_, i) => i);
+  cols = Array.from({ length: 10 }, (_, i) => i);
+  grid: unknown[][] = [];
+  selectedShips: Ships[] = [];
 
+  ngOnInit(): void {
+    for (let i = 0; i < this.rows.length; i++) {
+      this.grid[i] = [];
+      for (let j = 0; j < this.cols.length; j++) {
+        this.grid[i][j] = null; // Represents an empty cell
+      }
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  placeShip(row: number, col: number) {}
 }
