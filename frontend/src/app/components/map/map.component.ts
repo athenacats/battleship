093e03dd3a9b7dd9efdabb5902735f355e807e42
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-26 14:14:35 +0300

    Change color of cell once ship is placed

--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Ships } from 'src/app/data/shipdata';
 import { SelectedshipService } from 'src/app/services/selectedship.service';
 
+interface Cell {
+  value: number | null;
+  backgroundColor: string;
+}
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -10,14 +15,15 @@ import { SelectedshipService } from 'src/app/services/selectedship.service';
 export class MapComponent implements OnInit {
   rows = Array.from({ length: 10 }, (_, i) => i);
   cols = Array.from({ length: 10 }, (_, i) => i);
-  grid: any[][] = [];
+  grid: Cell[][] = [];
   selectedShip!: Ships;
+  shipArray: Ships[] = [];
 
   ngOnInit(): void {
     for (let i = 0; i < this.rows.length; i++) {
       this.grid[i] = [];
       for (let j = 0; j < this.cols.length; j++) {
-        this.grid[i][j] = null; // Represents an empty cell
+        this.grid[i][j] = { value: null, backgroundColor: 'var(--table)' }; // Represents an empty cell
       }
     }
   }
@@ -36,21 +42,31 @@ export class MapComponent implements OnInit {
     }
   }
 
-  /*placeShip(ship: Ships) {
-    if (this.canPlaceShip(ship, this.grid, row, col)) {
+  placeShip(row: number, col: number): void {
+    const currentShip = this.selectedShip;
+    if (!currentShip || currentShip.alowedNumberOfShips === 0) {
+      return; // No ship selected, do nothing
+    }
+
+    if (this.canPlaceShip(currentShip, this.grid, row, col)) {
       // Update the grid to mark ship positions (assuming shipLength and orientation are set)
-      if (ship.orientation === 'horizontal') {
-        for (let i = 0; i < ship.shipLength; i++) {
-          this.grid[row][col + i] = 1;
+      if (currentShip.orientation === 'Horizontal') {
+        for (let i = 0; i < currentShip.shipLength; i++) {
+          this.grid[row][col + i].value = 1;
+          this.grid[row][col + i].backgroundColor = 'var(--primaryT)';
         }
       } else {
-        for (let i = 0; i < ship.shipLength; i++) {
-          this.grid[row + i][col] = 1;
+        for (let i = 0; i < currentShip.shipLength; i++) {
+          this.grid[row + i][col].value = 1;
+          this.grid[row][col + i].backgroundColor = 'var(--primaryT)';
         }
       }
+      currentShip.alowedNumberOfShips -= 1;
+      console.log(currentShip.alowedNumberOfShips);
 
       // Add the placed ship to the selectedShips array
-      this.selectedShips.push(ship);
+      this.shipArray.push(currentShip);
+      console.log(this.shipArray);
 
       // Remove the placed ship from the available ships list (assuming you have a ships list)
       // this.availableShips = this.availableShips.filter((s) => s.id !== ship.id);
@@ -61,34 +77,38 @@ export class MapComponent implements OnInit {
     }
   }
 
-  canPlaceShip(ship: Ships, grid: number[][], row: number, col: number): boolean {
+  canPlaceShip(ship: Ships, grid: Cell[][], row: number, col: number): boolean {
+    console.log(row, col);
+    console.log(ship.shipLength);
     // Check if the ship can be placed at the specified position (row, col)
     if (
-      row < 0 ||
-      col < 0 ||
-      (ship.orientation === 'horizontal' && col + ship.shipLength > 10) ||
-      (ship.orientation === 'vertical' && row + ship.shipLength > 10)
+      (ship.orientation === 'Horizontal' && col + ship.shipLength > 10) ||
+      (ship.orientation === 'Vertical' && row + ship.shipLength > 10)
     ) {
       // Ship goes out of bounds
+      console.log('yes');
       return false;
     }
 
-    if (ship.orientation === 'horizontal') {
+    if (ship.orientation === 'Horizontal') {
       for (let i = 0; i < ship.shipLength; i++) {
-        if (grid[row][col + i] !== 0) {
+        if (grid[row][col + i].value !== null) {
+          console.log('yes');
           // Cell is already occupied by another ship
           return false;
         }
       }
     } else {
       for (let i = 0; i < ship.shipLength; i++) {
-        if (grid[row + i][col] !== 0) {
+        if (grid[row + i][col].value !== 0) {
+          console.log('yes');
           // Cell is already occupied by another ship
           return false;
         }
       }
     }
 
+    console.log('yes');
     return true; // Ship can be placed at the specified position
-  }*/
+  }
 }
