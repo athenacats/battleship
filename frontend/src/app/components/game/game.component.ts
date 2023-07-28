8e1f97031f661e14b617e0bbdd6cc64be5dd3396
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-28 10:11:38 +0300

    Write up logic for player attack and test

--- frontend/src/app/components/game/game.component.ts
+++ frontend/src/app/components/game/game.component.ts
@@ -12,6 +12,7 @@ export class GameComponent {
   grid: Cell[][] = [];
   AIgrid: AICell[][] = [];
   display = true;
+  isPlayerTurn = true;
 
   constructor(private initialiseGameService: InitialisegameService) {
     this.grid = this.initialiseGameService.getMapData();
@@ -20,4 +21,37 @@ export class GameComponent {
     console.log(this.grid);
     console.log(this.AIgrid);
   }
+
+  checkState(row: number, col: number): void {
+    if (this.isPlayerTurn) {
+      const cell = this.AIgrid[row][col];
+      if (!cell.attacked) {
+        cell.attacked = true;
+        if (cell.value === 1) {
+          cell.backgroundColor = 'var(--attacked)';
+          cell.value = null;
+          console.log(cell.value);
+        } else {
+          cell.backgroundColor = 'var(--miss)';
+          console.log(cell.value);
+        }
+
+        const allAIShipsSunk = this.AIgrid.every((row) => {
+          row.every((cell) => {
+            cell.value !== 1;
+          });
+        });
+        if (allAIShipsSunk) {
+          console.log('Player wins');
+          // figure out
+        }
+
+        this.isPlayerTurn = false;
+        this.aiAttack();
+      }
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  aiAttack() {}
 }
